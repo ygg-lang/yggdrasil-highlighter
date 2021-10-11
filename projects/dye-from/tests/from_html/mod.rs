b95b37c5dbe439d90150cb1100c31faa6a25f9db
Aster <galaster@foxmail.com>
2021-10-11 23:54:23 +0800

    🔮 Using simd html parsing

--- projects/dye-from/tests/from_html/mod.rs
+++ projects/dye-from/tests/from_html/mod.rs
@@ -1,12 +1,12 @@
-use tl::{HTMLTag, Node};
+use tl::{Attributes, HTMLTag, Node};
 
 #[test]
 fn test() {
     let dom = tl::parse(include_str!("highlightjs.in.html"), tl::ParserOptions::default()).unwrap();
+    let mut out = vec![];
     let parser = dom.parser();
     for block in dom.get_elements_by_class_name("hljs") {
-        HighlightJs::default();
-        parse_node(block.get(parser));
+        if let Some(s) = HighlightJs::parse_node(block.get(parser)) { out.push(s) }
     }
 }
 
@@ -14,21 +14,28 @@ impl HighlightJs {
     #[inline]
     fn parse_node(node: Option<&Node>) -> Option<Self> {
         let tag = node?.as_tag()?;
-        let mut out = HighlightJs::default();
-
-        if tag.name().eq("code") {
-            return None
-        }
-        let attr = tag.attributes();
-        attr.get("class")?;
-
-
-
         println!("{:#?}", tag);
-        Some(true)
+        let mut out = HighlightJs::default();
+        let attr = tag.attributes();
+        out.check_code(tag);
+        println!("{:#?}", out.extract_language(attr));
+
+        todo!()
+    }
+    fn check_code(&mut self, tag: &HTMLTag) -> Option<()> {
+        let is_code = tag.name().eq("code");
+        println!("is_code: {}", is_code);
+        None
+    }
+
+    fn extract_language(&mut self, attr: &Attributes) -> Option<()> {
+        let class = attr.get("class")??.as_utf8_str();
+
+
+        println!("{:#?}", class);
+        None
     }
 }
-
 
 
 #[derive(Debug, Default)]
